4d6da0dc28f1f201a4b2f30bb2c4e34c4bd8865a
JuanMM27 <juanartin9@gmail.com>
2025-06-16 16:40:57 -0300

    c

--- js/dinamicaPantallaPrincipal.js
+++ js/dinamicaPantallaPrincipal.js
@@ -1,9 +1,11 @@
 const SERIES_Y_PELICULAS = JSON.parse(localStorage.getItem("seriesYPeliculas"));
 const rutaActual = window.location.pathname;
+const selectCategoria = document.getElementById("categoria");
 //Nos trae en que parte de la url estamos, ej serie.html o pelicula.html
 
-function mostrarPeliculasYSeries(arrayDePeliculasYSeries){
+function mostrarPeliculasYSeries(arrayDePeliculasYSeries) {
     const nodoElement = document.querySelector(".grilla-contenido");
+    nodoElement.innerHTML = "";
     arrayDePeliculasYSeries.forEach(seriesYPeliculas => {
         const contenidoElement = document.createElement("a");
         contenidoElement.href = `./serie-pelicula.html?id=${seriesYPeliculas.id}`;
@@ -17,7 +19,7 @@ function mostrarPeliculasYSeries(arrayDePeliculasYSeries){
     //Recorriendo todo el array de objetos y por cada objeto crea su a con img
 }
 
-function mostrarPeliculasOSeriesSegunRuta(arrayDePeliculasYSeries, rutaActual){    
+function mostrarPeliculasOSeriesSegunRuta(arrayDePeliculasYSeries, rutaActual) {
     const filtrado = arrayDePeliculasYSeries.filter(seriesOPeliculas => seriesOPeliculas.tipo == rutaActual);
     //Filtramos el array para asegurarnos que es solo de series o solo de peliculas
     mostrarPeliculasYSeries(filtrado);
@@ -32,3 +34,25 @@ if (rutaActual.includes("serie")) {
 } else if (rutaActual.includes("pantalla-principal")) {
     mostrarPeliculasYSeries(SERIES_Y_PELICULAS); //Si no es solo pelicula o solo serie mostramos todo
 }
+
+selectCategoria.addEventListener("change", () => {
+    const valorElegido = selectCategoria.value.toLowerCase();
+    const filtradoPorCategoria = SERIES_Y_PELICULAS.filter(seriesOPeliculas => seriesOPeliculas.genero.toLowerCase() == valorElegido);
+
+    /*    if(valorElegido === ""){
+            mostrarPeliculasYSeries(SERIES_Y_PELICULAS);
+        }else{
+            
+            console.log("Películas filtradas:", filtradoPorCategoria);
+            mostrarPeliculasYSeries(filtradoPorCategoria);
+        }
+    */
+    if (rutaActual.includes("serie")) {
+        mostrarPeliculasOSeriesSegunRuta(filtradoPorCategoria, "serie"); //Si es serie pasamos el array y la palabra serie para ruta actual
+    } else if (rutaActual.includes("pelicula")) {
+        mostrarPeliculasOSeriesSegunRuta(filtradoPorCategoria, "pelicula"); //Si es serie pasamos el array la palabra pelicula para ruta actual
+    } else if (rutaActual.includes("pantalla-principal")) {
+        mostrarPeliculasYSeries(filtradoPorCategoria); //Si no es solo pelicula o solo serie mostramos todo
+    }
+
+});
