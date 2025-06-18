41180698d1e1d3dc7637117c5fd328e072c56d37
Matias Cantoni <matiascantoni@hotmail.com>
2025-06-18 12:59:33 -0300

    correo y usuario dinamicos
    hago que se vea el correo y el nombre de usuario en la pagina de perfil de manera dinamica

--- js/perfil.js
+++ js/perfil.js
@@ -1,5 +1,19 @@
 const cerrarSesion = document.querySelector(".cerrar-sesion");
+//email-js
 
-cerrarSesion.addEventListener("click", function(){
+cerrarSesion.addEventListener("click", function () {
     localStorage.removeItem("usuarioActivo");
 })
+
+// --- correo electronico y usuario dinamicamente ---
+let textoEmail = document.querySelector(".email-js");
+let textoUsuario = document.querySelector(".usuario-js");
+const usuarioActual = JSON.parse(localStorage.getItem("usuarioActivo")); //agarramos el usuario activo
+let correoActual = usuarioActual.correo; // guardamos el correo del usuario en un let
+let nombreUsuarioActual = usuarioActual.usuario; // guardamos el nombre de usuario en un let
+
+textoEmail.textContent = correoActual;  // hacemos que aparezca en pantalla el correo en modo de texto
+textoUsuario.textContent = nombreUsuarioActual; // hacemos que aparezca en pantalla el nombre del usuario en modo de texto
+
+
+
